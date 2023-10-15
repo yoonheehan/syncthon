9f85b450db3dd3a7f0e5ca9023d06b3025152870
silvermaking <silvermaking@gmail.com>
2023-10-15 23:50:53 +0900

    feat: gpt 답변 호출 구현

--- app/plan/travelPlan.js
+++ app/plan/travelPlan.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 
 const TravelPlan = () => {
   const router = useRouter();
+  const [resultAnswer, setResultAnswer] = useState(undefined);
 
   useEffect(() => {
     let balance = localStorage.getItem("balance");
@@ -12,8 +13,9 @@ const TravelPlan = () => {
       router.push("/");
       return;
     }
+    console.log("eseses");
 
-    let totalSentence = `당신은 여행 플래너야. 통장 잔액 ${balance}만원으로 대한민국 국내 여행 계획을 세워줘. 인근 맛집도 추천해주면 베스트!`;
+    let totalSentence = `당신은 여행 플래너야. 통장 잔액 ${balance}원으로 대한민국 국내 여행 계획을 세워줘. 인근 맛집도 추천해주면 베스트!`;
     const getAnswer = async () => {
       let a = await fetch("api/gptapi", {
         method: "POST",
@@ -23,15 +25,19 @@ const TravelPlan = () => {
         body: JSON.stringify({
           prompt: totalSentence,
         }),
-      });
-      console.log("eseses" + a);
+      }).then((res) => res.json());
+
+      console.log("eseses" + a.result.choices[0].text);
+      const resultText = a.result.choices[0].text;
+      setResultAnswer(resultText);
     };
     getAnswer();
-  }, [router]);
+  }, []);
 
   return (
     <>
       <div>Enterdddddddddddddddddd</div>
+      <div>{resultAnswer}</div>
     </>
   );
 };
