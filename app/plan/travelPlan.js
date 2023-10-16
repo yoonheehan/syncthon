f5932ec84d3462f89a10ee5822457b5d9de29754
silvermaking <silvermaking@gmail.com>
2023-10-16 23:37:30 +0900

    feat: 여행계획 세우기 꾸미기 구현

--- app/plan/travelPlan.js
+++ app/plan/travelPlan.js
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 
 const TravelPlan = () => {
   const router = useRouter();
-  const [resultAnswer, setResultAnswer] = useState(undefined);
+  const [resultAnswer, setResultAnswer] = useState([]);
 
   useEffect(() => {
     let balance = localStorage.getItem("balance");
@@ -15,7 +15,7 @@ const TravelPlan = () => {
     }
     console.log("eseses");
 
-    let totalSentence = `당신은 여행 플래너야. 통장 잔액 ${balance}원으로 대한민국 국내 여행 계획을 세워줘. 인근 맛집도 추천해주면 베스트!`;
+    let totalSentence = `당신은 여행 플래너야.통장 잔액 ${balance}원으로 여행 계획을 1500자로 써줘. 돈이 100만원 이상이면 해외로, 100만원 이하면 국내로 추천해줘`;
     const getAnswer = async () => {
       let a = await fetch("api/gptapi", {
         method: "POST",
@@ -29,15 +29,26 @@ const TravelPlan = () => {
 
       console.log("eseses" + a.result.choices[0].text);
       const resultText = a.result.choices[0].text;
-      setResultAnswer(resultText);
+      setResultAnswer(resultText.split("\n\n"));
     };
     getAnswer();
   }, []);
 
   return (
     <>
-      <div>Enterdddddddddddddddddd</div>
-      <div>{resultAnswer}</div>
+      <div className="p-4 space-y-4 bg-gradient-to-r from-blue-400 to-purple-500 min-h-screen flex flex-col items-center justify-center">
+        {resultAnswer.map(
+          (answer, index) =>
+            index > 1 && (
+              <div
+                key={index}
+                className="relative bg-white text-gray-800 p-4 rounded-lg my-2 w-full inline-block max-w-2xl shadow-lg"
+              >
+                <span>{answer}</span>
+              </div>
+            )
+        )}
+      </div>
     </>
   );
 };
