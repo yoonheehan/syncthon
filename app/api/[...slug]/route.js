fc8f44bfd6f481a2b12869fb6a779851cb47e0b5
yoonheehan <grayman1234@naver.com>
2023-10-16 22:17:29 +0900

    gpt 수정

--- app/api/[...slug]/route.js
+++ app/api/[...slug]/route.js
@@ -16,7 +16,7 @@ export async function POST(request, { params }) {
   const res = await request.json();
   console.log(res);
   console.log(params);
-  const apiUrl = `${process.env.MY_SYNCTHONS}/${params.slug[0]}`;
+  const apiUrl = `${process.env.SILVER_SYNCTHONS}/${params.slug[0]}`;
   console.log(apiUrl);
   const requestOptions = {
     method: "POST",
